b9411cc0a020eb348941b073ac2c92418dd624d5
Munazzabegam <4pa21ec037@pace.edu.in>
2023-11-14 17:14:17 +0530

    updated

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 function copyToClipboard(commandId) {
-    var copyText = document.getElementById(commandId);
+    var copyText = document.getElementById(commandId).cloneNode(true);
+
+
+    var spanElement = copyText.querySelector('span');
+    if (spanElement) {
+        spanElement.parentNode.removeChild(spanElement);
+    }
+
     var textArea = document.createElement("textarea");
-    textArea.value = copyText.textContent.trim(); // Trim whitespace
+    textArea.value = copyText.textContent.trim();
     document.body.appendChild(textArea);
     textArea.select();
     document.execCommand("Copy");
-    document.body.removeChild(textArea);
-    alert("Command copied to clipboard!");
+
+    // document.body.removeChild(textArea);
+    // alert("Command copied to clipboard!");
 }
